f0ed17fafd8a4596f16106092840ffd29aa803f3
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2021-05-23 15:12:24 +0800

    added logger

--- khron_brownie/contracts/khron_logger.sol
+++ khron_brownie/contracts/khron_logger.sol
@@ -5,6 +5,11 @@ contract khron_logger {
     
     event Transfer (address indexed _to, string data);
 
+    
+    function checking() public pure returns(bytes4) {
+        return this.transfer_eth.selector;
+    }
+
     function transfer_eth(address _to, string memory data) public payable {
         emit Transfer(_to, data);
     }
