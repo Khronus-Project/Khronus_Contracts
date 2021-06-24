c6fbb8182f631ba2f144c93e2afd20335cce8c1d
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2021-06-24 15:16:32 +0800

    added new script to deploy contracts to ropsten

--- khron_brownie/scripts/send_events.py
+++ khron_brownie/scripts/send_events.py
@@ -2,13 +2,15 @@ from brownie import accounts, TestKhronusClient, TestKhronusNode
 import eth_event
 import json
 
+#Hex 'hello world' 68656c6c6f20776f726c64
+#Hex 'this is real' 74686973206973207265616c
 
 def main():
     with open ('../contract_library/contract_addresses.json') as f:
         addresses = json.load(f)
     with open ('build/contracts/KhronusClient.json') as f:
         abiJson = json.load(f)
-    tx = TestKhronusClient.at(addresses['TestKhronusClient']).setKhronTab(TestKhronusNode.at(addresses['TestKhronusNode']),10**18,'68656c6c6f20776f726c64',{'from':accounts[0]})
+    tx = TestKhronusClient.at(addresses['TestKhronusClient']).setKhronTab(TestKhronusNode.at(addresses['TestKhronusNode']),10**18,'74686973206973207265616c',{'from':accounts[0]})
     data = []
     for log in tx.logs:
         dir = {}
