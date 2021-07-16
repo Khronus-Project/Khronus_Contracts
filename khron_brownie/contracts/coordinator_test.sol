c450bebc86c4a2935968e5d6096b801355a7a093
Daniel Herrera <Mayan.Bridge.Consulting@Gmail.com>
2021-07-16 17:13:48 +0800

    finished feature (#1)

---
+++ khron_brownie/contracts/coordinator_test.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "contracts/coordinator_base/KhronusCoordinator.sol";
+
+contract TestKhronusCoordinator is KhronusCoordinator{
+}
+
+
--- khron_brownie/contracts/node_test.sol
+++ khron_brownie/contracts/node_test.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./nodes/abstracts/KhronusNode.sol";
+import "./node_base/KhronusNode.sol";
 
 contract TestKhronusNode is KhronusNode {
 }
