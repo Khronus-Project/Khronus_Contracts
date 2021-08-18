40136fbe5d7e3f4509e843af6931c1b654d049ca
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2021-08-18 14:31:17 +0800

    changed request to 2 minutes

--- khron_brownie/scripts/send_khron_request.py
+++ khron_brownie/scripts/send_khron_request.py
@@ -7,7 +7,7 @@ def main():
     # Constants
     escrowDepositor = accounts[2]
     escrowBeneficiary = accounts[3]
-    timestamp = int(datetime.utcnow().timestamp())+180
+    timestamp = int(datetime.utcnow().timestamp())+120
     with open ('../contract_library/contract_addresses.json') as f:
         addresses = json.load(f)
     clientContract = EscrowInfrastructure.at(addresses["KhronusClient"])
