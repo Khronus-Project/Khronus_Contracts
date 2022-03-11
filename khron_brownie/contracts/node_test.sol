9d403a62d19492060bc15c5535847c08cff197de
Daniel Herrera <Mayan.Bridge.Consulting@Gmail.com>
2022-03-11 22:20:06 +0800

    Feature khron job design (#19)
    * checking gas left strategy
    
    * fixing local
    
    * generalized imports to repo sources when applicable and made changes as design consideration for khron-tabs

--- khron_brownie/contracts/node_test.sol
+++ khron_brownie/contracts/node_test.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./KhronusNodeBase.sol";
+//Local Import from package source
+//import "Khronus_Utils/contracts/KhronusNodeBase.sol";
+//Remote import from repo
+import "@khronus/khronus-utils@0.0.1/contracts/KhronusNodeBase.sol";
 
 contract TestKhronusNode is KhronusNode {
     constructor (address _khronus_Coordinator) KhronusNode(_khronus_Coordinator) {
